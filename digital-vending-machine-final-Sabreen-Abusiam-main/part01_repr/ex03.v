c71c76578dafe69aeebd4e73bbdf06ee7f9bdbe4
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 09:55:00 +0200

    Finish ex03

--- digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex03.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex03.v
@@ -1,7 +1,14 @@
+\
 // ex03.v - Hexadecimal to binary conversions
 // WHAT TO DO: Understand that hex and binary are just different ways to write the same number.
 // This exercise demonstrates that 4-bit values can be written as hex (0-F) or binary (0000-1111).
 module ex03(
+input  [3:0] hex_value,
+output [3:0] binary_value
+);
+
+assign binary_value = hex_value;
+
 
 
 endmodule
