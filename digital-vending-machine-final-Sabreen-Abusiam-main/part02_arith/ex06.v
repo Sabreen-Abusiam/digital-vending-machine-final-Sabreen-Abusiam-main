2a759fb9bc59772966e8edacc9a35975886a9b31
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 10:21:48 +0200

    Finish ex06

--- digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex06.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex06.v
@@ -2,5 +2,11 @@
 // This demonstrates multi-bit signals and arithmetic operations
 
 module ex06(
-  
+    input  [3:0] coin_a,
+    input  [3:0] coin_b,
+    output [4:0] sum
+);
+
+assign sum = coin_a + coin_b;
+
 endmodule
