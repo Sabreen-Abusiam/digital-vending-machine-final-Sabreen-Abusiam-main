8a6ba200a8344b2f648b857afea2b76070e96a6b
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 09:26:28 +0200

    Finish ex02

--- digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex02.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex02.v
@@ -2,6 +2,13 @@
 // WHAT TO DO: Learn how to represent decimal numbers in binary using 4 bits.
 // This exercise shows you how vending machine prices (0-15 NIS) are stored in hardware.
 module ex02(
+    input  [3:0] price_decimal,  // decimal price (0-15)
+    output [3:0] price_binary     // binary representation
+);
 
+assign price_binary = price_decimal;
 
 endmodule
+
+
+
