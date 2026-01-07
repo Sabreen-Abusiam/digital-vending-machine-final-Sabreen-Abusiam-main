72657b08aed87987c059153f87e6f413e2f35b40
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 10:24:51 +0200

    Finish ex08

--- digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex08.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex08.v
@@ -2,7 +2,12 @@
 // WHAT TO DO: Learn how negative numbers are represented in hardware using 2's complement.
 // This teaches you how to negate a number and detect if a number is negative.
 module ex08(
+    input  signed [4:0] in_value,
+    output signed [4:0] neg_value,
+    output               is_negative
+);
 
+assign neg_value   = -in_value;
+assign is_negative = in_value[4];
 
 endmodule
-
