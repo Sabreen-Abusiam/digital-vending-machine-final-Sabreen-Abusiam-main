b055d49e68ceabd8c309e0b3f44ca177df355852
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 10:27:42 +0200

    Finish ex09

--- digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex09.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex09.v
@@ -2,9 +2,16 @@
 // WHAT TO DO: Detect when adding two numbers produces a result too large to fit in 5 bits.
 // Overflow detection prevents errors when accumulating coins in the vending machine.
 module ex09(
- 
+    input  [4:0] a,
+    input  [4:0] b,
+    output [4:0] sum,
+    output       overflow
 );
 
+wire [5:0] full_sum;
+
+assign full_sum = a + b;
+assign sum      = full_sum[4:0];
+assign overflow = full_sum[5];
 
 endmodule
-
