7d5299866bd385ef3b9d0ef37bd69fd0c1335a19
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 10:14:01 +0200

    Finish ex04

--- digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex04.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex04.v
@@ -2,5 +2,12 @@
 // WHAT TO DO: Create logic that validates if an inserted coin is legal (1, 5, or 10 NIS).
 // This is your first decision-making circuit - it checks conditions and outputs valid/invalid.
 module ex04(
+    input  [3:0] coin_input,
+    output        is_valid
+);
+
+assign is_valid = (coin_input == 4'd1) ||
+                  (coin_input == 4'd5) ||
+                  (coin_input == 4'd10);
 
 endmodule
