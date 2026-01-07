687bba11fff1e0183e449121e860aa49de4a20ea
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 10:53:19 +0200

    Finish ex17

--- digital-vending-machine-final-Sabreen-Abusiam-main/part04_kmap/ex17.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part04_kmap/ex17.v
@@ -2,11 +2,16 @@
 // WHAT TO DO: Implement a more complex function simplified from a 4-variable K-map.
 // This shows how K-maps scale to handle more inputs while still minimizing logic.
 module ex17(
- 
+    input A,
+    input B,
+    input C,
+    input D,
+    output F
 );
 
-  // F = (~B&~D) | (~B&~C) | (~A&~C&D)
+assign F = (~B & ~D) | (~B & C) | (~A & ~C & D);
 
+  // F = (~B&~D) | (~B&~C) | (~A&~C&D)
 
 endmodule
 
