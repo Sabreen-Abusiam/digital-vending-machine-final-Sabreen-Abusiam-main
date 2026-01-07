7975e95230132bd2da1d5975536894abbee283db
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 20:03:40 +0200

    Finish ex18

--- digital-vending-machine-final-Sabreen-Abusiam-main/part04_kmap/ex18.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part04_kmap/ex18.v
@@ -2,11 +2,13 @@
 // WHAT TO DO: Use "don't care" conditions to achieve maximum simplification of logic.
 // Don't cares represent inputs that will never occur, allowing even simpler circuits.
 module ex18(
-
+    input A,
+    input B,
+    input C,
+    output F
 );
 
+assign F = C;
   // Simplified to: F = C
-
-
 
 endmodule
