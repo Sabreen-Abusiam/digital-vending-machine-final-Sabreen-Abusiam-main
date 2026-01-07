8374184350c7718a5a4db7b32f5039f3dd0c81bc
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-07 21:20:36 +0200

    Finish ex24

--- digital-vending-machine-final-Sabreen-Abusiam-main/part05_seq/ex24.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part05_seq/ex24.v
@@ -2,8 +2,14 @@
 // WHAT TO DO: Build a multi-bit register that stores the total amount of money inserted.
 // This register is the core of the coin accumulator in your vending machine.
 module ex24(
-
+    input  [4:0] D,
+    input        clk,
+    output reg [4:0] Q
 );
+
+always @(posedge clk) begin
+    Q <= D;
+end
 
 endmodule
 
