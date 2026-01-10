3600914facad9d3fe773a269ff8fab82a9fd0415
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-10 20:46:21 +0200

    Finish ex15

--- digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex15.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex15.v
@@ -2,9 +2,13 @@
 // WHAT TO DO: Use the consensus theorem to eliminate redundant terms in Boolean expressions.
 // This optimization reduces the number of gates needed in your circuit.
 module ex15(
- 
+    input  A,
+    input  B,
+    input  C,
+    output F
 );
 
+    assign F = (A & B) | (~A & C);
 
 endmodule
 
