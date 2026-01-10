71c1ba0329d32222e3a42fddda6484a7b0887594
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-10 19:29:55 +0200

    Finish ex05

--- digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex05.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part01_repr/ex05.v
@@ -2,6 +2,13 @@
 // WHAT TO DO: Learn why we need 5 bits to store amounts up to 31 NIS in the vending machine.
 // This exercise introduces overflow detection - what happens when the amount is too large.
 module ex05(
-
-
+    input  [4:0] amount,
+    output [4:0] stored_amount,
+    output       overflow
+);
+    assign stored_amount = amount;
+    assign overflow = 1'b0;
 endmodule
+
+
+
