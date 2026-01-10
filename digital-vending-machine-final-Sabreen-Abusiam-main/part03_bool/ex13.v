1b0024288229d79ef558ed17e59a10defcc1e04b
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-10 20:40:22 +0200

    Finish ex13

--- digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex13.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex13.v
@@ -2,9 +2,14 @@
 // WHAT TO DO: Learn the basic rules of Boolean algebra (A OR 0 = A, A AND 1 = A, etc.).
 // These fundamental laws help you simplify complex logic circuits.
 module ex13(
-
+    input  A,
+    output F1, F2, F3, F4
 );
 
+    assign F1 = A | 1'b0;
+    assign F2 = A & 1'b1;
+    assign F3 = A | 1'b1;
+    assign F4 = A & 1'b0;
 
 endmodule
 
