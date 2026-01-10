11dfe7b2c6af2048259a287a80394a47c9486ebc
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-10 20:34:41 +0200

    Finish ex12

--- digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex12.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex12.v
@@ -2,6 +2,13 @@
 // WHAT TO DO: Express the same logic function as Product of Sums (AND of ORs).
 // You'll see that the same function can be written in different but equivalent ways.
 module ex12(
-
+    input A,
+    input B,
+    input C,
+    output F
 );
- endmodule
+
+    assign F = (A | B | C) & (~A | B | ~C);
+
+endmodule
+
