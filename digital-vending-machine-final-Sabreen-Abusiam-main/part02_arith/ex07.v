c7677b38932acd5ecde6d7ceda0d933d9dbdcc50
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-10 20:09:16 +0200

    Finish ex07

--- digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex07.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex07.v
@@ -2,11 +2,11 @@
 // WHAT TO DO: Implement subtraction to calculate change (money paid minus product price).
 // This is essential for the vending machine to return the correct change to customers.
 module ex07(
-    input  [4:0] money_paid,
-    input  [4:0] product_price,
-    output [4:0] change
+  input  [4:0] paid,
+  input  [3:0] price,
+  output [4:0] change
 );
 
-assign change = money_paid - product_price;
+  assign change = paid - price;
 
 endmodule
