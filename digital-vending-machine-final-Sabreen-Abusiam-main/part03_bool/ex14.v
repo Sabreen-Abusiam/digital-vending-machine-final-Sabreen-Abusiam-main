b78e8f8e5321765253ba3cb9f00e4cb42a4eccb6
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-10 20:43:37 +0200

    Finish ex14

--- digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex14.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part03_bool/ex14.v
@@ -2,10 +2,23 @@
 // WHAT TO DO: Apply De Morgan's theorem to convert between AND/OR operations with NOT.
 // This powerful theorem lets you transform logic expressions to find simpler implementations.
 module ex14(
-
+  input  A,
+  input  B,
+  input  C,
+  output F1,
+  output F2,
+  output F3
 );
 
- 
+  // F1 = ~(A & B) | C   (De Morgan idea: ~(A&B) = ~A | ~B)
+  assign F1 = ~(A & B) | C;
+
+  // F2 = ~(A & B) & ~C
+  assign F2 = ~(A & B) & ~C;
+
+  // Absorption / simplification:
+  // A&B + A&~B = A*(B + ~B) = A*1 = A
+  assign F3 = A;
 
 endmodule
 
