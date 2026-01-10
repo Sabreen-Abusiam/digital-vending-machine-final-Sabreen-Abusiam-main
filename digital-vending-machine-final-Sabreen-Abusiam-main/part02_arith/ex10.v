330e33dd6d4a1abac577ec70013c5e125aefe8e2
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-10 20:27:25 +0200

    Finish ex10

--- digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex10.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part02_arith/ex10.v
@@ -2,10 +2,14 @@
 // WHAT TO DO: Build a comparator that checks if the customer paid enough for their product.
 // This circuit outputs true when paid amount is greater than or equal to the price.
 module ex10(
- 
+
+    input  [4:0] paid,
+    input  [3:0] price,
+    output        is_sufficient
 );
 
-
+    assign is_sufficient = (paid >= price);
 
 endmodule
+ 
 
