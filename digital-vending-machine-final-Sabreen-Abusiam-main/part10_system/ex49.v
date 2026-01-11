5cd3a3e7f4c4eb1c0d32054901921bec1b8bc022
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 09:17:24 +0200

    Finish ex49

--- digital-vending-machine-final-Sabreen-Abusiam-main/part10_system/ex49.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part10_system/ex49.v
@@ -2,8 +2,18 @@
 // WHAT TO DO: Use RAM to track product inventory, decrementing stock when items are dispensed.
 // This adds realistic inventory management to your vending machine.
 module ex49(
-  
+    input        clk,
+    input        rst,
+    input  [1:0] product,
+    input        dispense,
+    output reg [3:0] stock
 );
 
-endmodule
+always @(posedge clk) begin
+    if (rst)
+        stock <= 4'd10;
+    else if (dispense && stock > 0)
+        stock <= stock - 1;
+end
 
+endmodule
