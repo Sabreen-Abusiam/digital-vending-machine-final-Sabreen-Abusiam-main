864687ae68f39872651c696acb582209c40093e2
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 09:14:09 +0200

    Finish ex48

--- digital-vending-machine-final-Sabreen-Abusiam-main/part10_system/ex48.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part10_system/ex48.v
@@ -2,10 +2,17 @@
 // WHAT TO DO: Create Read-Only Memory that stores fixed product prices.
 // ROM is perfect for constant data that never changes, like price lists.
 module ex48(
-  
+    input  [1:0] addr,
+    output reg [3:0] price
 );
 
-
+always @(*) begin
+    case (addr)
+        2'd0: price = 4'd5;
+        2'd1: price = 4'd10;
+        2'd2: price = 4'd15;
+        default: price = 4'd0;
+    endcase
+end
 
 endmodule
-
