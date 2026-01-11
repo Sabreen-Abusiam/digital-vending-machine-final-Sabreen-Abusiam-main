1de4a93a211198c7de765b49791d58b63131b610
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 08:39:29 +0200

    Finish ex39

--- digital-vending-machine-final-Sabreen-Abusiam-main/part08_arith/ex39.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part08_arith/ex39.v
@@ -2,10 +2,16 @@
 // WHAT TO DO: Build a subtractor circuit to calculate change (paid - price).
 // Subtraction can be implemented using addition with 2's complement.
 module ex39(
- 
+    input  [4:0] a,
+    input  [4:0] b,
+    output [4:0] diff,
+    output       borrow
 );
 
+wire [5:0] tmp;
 
+assign tmp    = {1'b0, a} + {1'b0, ~b} + 6'd1;
+assign diff   = tmp[4:0];
+assign borrow = ~tmp[5];
 
 endmodule
-
