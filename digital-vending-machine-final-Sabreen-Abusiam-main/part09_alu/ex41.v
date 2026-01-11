0df34548a13f9dd3f2edc11e98d1ff8596cb2a69
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 08:50:15 +0200

    Finish ex41

--- digital-vending-machine-final-Sabreen-Abusiam-main/part09_alu/ex41.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part09_alu/ex41.v
@@ -2,10 +2,17 @@
 // WHAT TO DO: Build your first Arithmetic Logic Unit that performs ADD or SUB based on opcode.
 // ALUs are the computational heart of processors - this is a simplified version.
 module ex41(
- 
+    input  [4:0] a,
+    input  [4:0] b,
+    input        op,
+    output [4:0] result
 );
 
+    wire [4:0] b_mod;
+    wire [5:0] sum_ext;
 
+    assign b_mod   = op ? ~b : b;
+    assign sum_ext = {1'b0, a} + {1'b0, b_mod} + op;
+    assign result  = sum_ext[4:0];
 
 endmodule
-
