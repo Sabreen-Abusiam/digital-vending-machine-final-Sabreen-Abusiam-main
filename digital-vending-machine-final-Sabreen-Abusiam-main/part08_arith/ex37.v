9565811fc7c98249744d7d870a94376dbd990735
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 08:32:58 +0200

    Finish ex37

--- digital-vending-machine-final-Sabreen-Abusiam-main/part08_arith/ex37.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part08_arith/ex37.v
@@ -2,9 +2,14 @@
 // WHAT TO DO: Extend the half adder to include a carry input for chaining multiple adders.
 // Full adders can be cascaded to create adders of any bit width.
 module ex37(
- 
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
 );
 
+assign sum  = a ^ b ^ cin;
+assign cout = (a & b) | (a & cin) | (b & cin);
 
 endmodule
-
