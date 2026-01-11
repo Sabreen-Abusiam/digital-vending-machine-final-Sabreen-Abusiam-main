b77794869c28dc653b92656d465ec6fbbbc43c67
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 08:18:16 +0200

    Finish ex33

--- digital-vending-machine-final-Sabreen-Abusiam-main/part07_impl/ex33.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part07_impl/ex33.v
@@ -2,6 +2,7 @@
 // WHAT TO DO: Build a controlled dispenser that only releases products when payment is confirmed.
 // This prevents giving away free products - dispense only when both request AND payment are valid.
 module ex33(
+
     input  clk,
     input  rst,
     input  dispense_req,
