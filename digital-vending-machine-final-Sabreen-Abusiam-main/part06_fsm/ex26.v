6e053aa0e8583dccd4a624fbf5fed13a0c5cbb1f
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 07:38:29 +0200

    Finish ex26

--- digital-vending-machine-final-Sabreen-Abusiam-main/part06_fsm/ex26.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part06_fsm/ex26.v
@@ -3,9 +3,23 @@
 // Like a traffic light or vending machine controller
 
 module ex26(
+    input clk,
+    input rst,
+    input start,
+    output reg state
 
 );
 
   // States: 0 = IDLE, 1 = ACTIVE
+    initial state = 1'b0;
+
+    always @(posedge clk) begin
+        if (rst)
+            state <= 1'b0;
+        else if (start)
+            state <= 1'b1;
+        else
+            state <= state;
+    end
 
 endmodule
