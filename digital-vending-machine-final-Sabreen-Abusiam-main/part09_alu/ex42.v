19d262a739f43d18c00226f10c8f2c7bb9bca44c
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 08:54:05 +0200

    Finish ex42

--- digital-vending-machine-final-Sabreen-Abusiam-main/part09_alu/ex42.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part09_alu/ex42.v
@@ -2,9 +2,20 @@
 // WHAT TO DO: Expand your ALU to perform four operations: arithmetic (ADD/SUB) and logic (AND/OR).
 // Use a 2-bit opcode to select which operation to perform.
 module ex42(
-
+    input  [4:0] a,
+    input  [4:0] b,
+    input  [1:0] op,
+    output reg [4:0] result
 );
 
+always @(*) begin
+    case (op)
+        2'b00: result = a + b;
+        2'b01: result = a - b;
+        2'b10: result = a & b;
+        2'b11: result = a | b;
+        default: result = 5'd0;
+    endcase
+end
 
 endmodule
-
