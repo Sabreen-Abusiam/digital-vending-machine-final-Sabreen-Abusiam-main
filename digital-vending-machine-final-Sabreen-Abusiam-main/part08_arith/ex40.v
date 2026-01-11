f4c95af8042a08516ea321a987b1ae5d19895e1b
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 08:42:37 +0200

    Finish ex40

--- digital-vending-machine-final-Sabreen-Abusiam-main/part08_arith/ex40.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part08_arith/ex40.v
@@ -2,9 +2,20 @@
 // WHAT TO DO: Create one circuit that can both add and subtract based on a control signal.
 // This is more efficient than having separate adder and subtractor circuits.
 module ex40(
-
+    input  [4:0] a,
+    input  [4:0] b,
+    input        sub,
+    output [4:0] result,
+    output       carry_borrow
 );
 
+    wire [4:0] b_mod;
+    wire [5:0] sum_ext;
+
+    assign b_mod = sub ? ~b : b;
+    assign sum_ext = {1'b0, a} + {1'b0, b_mod} + sub;
+
+    assign result = sum_ext[4:0];
+    assign carry_borrow = sum_ext[5];
 
 endmodule
-
