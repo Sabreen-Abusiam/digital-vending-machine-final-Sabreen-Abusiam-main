073405505c1903b297e1384c551656afe8bc8eef
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 07:30:50 +0200

    Finish ex25

--- digital-vending-machine-final-Sabreen-Abusiam-main/part05_seq/ex25.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part05_seq/ex25.v
@@ -2,10 +2,23 @@
 // WHAT TO DO: Combine load (enable) and clear (reset) controls in one register.
 // This gives you full control over when to store new values and when to reset to zero.
 module ex25(
- 
+    input        clk,
+    input        load,
+    input        clear,
+    input  [4:0] d,
+    output reg [4:0] q
 );
 
+    initial q = 5'b00000;
 
+    always @(posedge clk) begin
+        if (clear)
+            q <= 5'b00000;
+        else if (load)
+            q <= d;
+        else
+            q <= q;   // hold value
+    end
 
 endmodule
 
