1ecbeddd58d9ab3b60e3cc05fddd88740655a825
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 09:08:05 +0200

    Finish ex46

--- digital-vending-machine-final-Sabreen-Abusiam-main/part10_system/ex46.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part10_system/ex46.v
@@ -2,10 +2,20 @@
 // WHAT TO DO: Create your first Random Access Memory to store and retrieve data.
 // RAM uses an address to select which location to read from or write to.
 module ex46(
- 
+    input        clk,
+    input        we,
+    input  [1:0] addr,
+    input  [3:0] data_in,
+    output [3:0] data_out
 );
 
-9
+    reg [3:0] mem [0:3];
+
+    always @(posedge clk) begin
+        if (we)
+            mem[addr] <= data_in;
+    end
+
+    assign data_out = mem[addr];
 
 endmodule
-
