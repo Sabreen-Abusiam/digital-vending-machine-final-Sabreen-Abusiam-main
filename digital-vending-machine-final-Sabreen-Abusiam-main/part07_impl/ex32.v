17182b1cca9d88cac2dfad520c7ae590d0071aa2
Sabreen-Abusiam <b1405627@mail.sapir.ac.il>
2026-01-11 08:13:39 +0200

    Finish ex32

--- digital-vending-machine-final-Sabreen-Abusiam-main/part07_impl/ex32.v
+++ digital-vending-machine-final-Sabreen-Abusiam-main/part07_impl/ex32.v
@@ -2,9 +2,22 @@
 // WHAT TO DO: Create a clocked circuit that continuously checks if payment is sufficient.
 // This validates that the customer has inserted enough money before allowing a purchase.
 module ex32(
-
+    input clk,
+    input rst,
+    input [4:0] paid,
+    input [3:0] price,
+    output reg valid
 );
 
+always @(posedge clk) begin
+    if (rst)
+        valid <= 1'b0;
+    else begin
+        if (paid >= price)
+            valid <= 1'b1;
+        else
+            valid <= 1'b0;
+    end
+end
 
 endmodule
-
